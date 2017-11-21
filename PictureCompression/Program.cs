25fe3c19bccfc7a27ea8882d4bf9cce9b6ad5e0f
zhang_john <muou03@gmail.com>
2017-11-21 15:27:20 +0800

    图片压缩DEMO

---
+++ PictureCompression/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureCompression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = @"D:\Temp\test.jpg";
+            var distination = @"D:\Temp\test_thumb.jpg";
+            var dHeight = 300;
+            var dWidth = 300;
+            var flag = 50;
+            ImageCompressor.Compress(source, distination, dHeight, dWidth, flag);
+        }
+    }
+}
